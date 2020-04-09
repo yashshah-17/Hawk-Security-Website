ba91f08f5afb52a475a140fdf48bb4ceb0a0c763
Yash Shah <shahyash1707@gmail.com>
2020-04-09 02:22:23 -0300

    Final Commit

--- app.js
+++ app.js
@@ -53,10 +53,10 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('frontend/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.json(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.json(__dirname, 'frontend', 'build', 'index.html'));
+//   });
+// }
